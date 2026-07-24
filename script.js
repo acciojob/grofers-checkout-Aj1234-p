985cee289bf1c9c1b2ba59add2653056c871cf8a
Ajit Pandey <ajitytsurat0707@gmail.com>
2026-07-24 19:47:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 const getSumBtn = document.createElement("button");
-const priceItem = document.querySelectorAll('.price');
-const table  = document.querySelector('table');
+ const priceItem = document.querySelectorAll('.price');
+// const table  = document.querySelector('table');
+const prices = document.querySelector('.prices');
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-	let sum =0;
+	let totalPrice =0;
 	priceItem.forEach((p)=>{
-    let money = Number(p.textContent);
-		sum+=money;
+    let price = Number(p.textContent);
+		totalPrice+=price;
 	})
-	let newRow = document.createElement('tr');
-	let newRowData = document.createElement('td');
-    newRowData.setAttribute('class','prices');
-	newRowData.textContent = sum;
-	newRow.appendChild(newRowData);
-    console.log(newRow)
-	table.appendChild(newRow);
+	// let newRow = document.createElement('tr');
+	// let newRowData = document.createElement('td');
+ //    newRowData.setAttribute('class','prices');
+	// newRowData.textContent = sum;
+	// newRow.appendChild(newRowData);
+ //    console.log(newRow)
+	// table.appendChild(newRow);
+	prices.textContent = totalPrice;
 };
 
 getSumBtn.addEventListener("click", getSum);
