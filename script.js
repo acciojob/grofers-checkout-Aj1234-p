f2fba646fe39c1fd442e1f49db38397553af9c78
Ajit Pandey <ajitytsurat0707@gmail.com>
2026-07-24 19:20:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const getSumBtn = document.createElement("button");
+const priceItem = document.querySelector('.price');
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+	let sum =0;
+	priceItem.forEach((p)=>{
+		p = parseInt(p);
+		sum+=p;
+	})
+	return sum;
 };
 
 getSumBtn.addEventListener("click", getSum);
